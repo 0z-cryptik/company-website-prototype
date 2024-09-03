b1727374ab93e0a548ff922dce78cc114de0b87a
Eniitan <enitanolawale0@gmail.com>
2024-09-03 13:06:14 +0100

    made the navbar stick to the top of the page

--- app/ui/navbar/navbar.tsx
+++ app/ui/navbar/navbar.tsx
@@ -1,9 +1,28 @@
-import { bebasNeue } from "../fonts";
+"use client";
+
+import { useEffect, useState } from "react";
 import { Logo } from "../logo/logo";
 
 export const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="pt-7 pl-[3rem] pr-[7rem] flex flex-row">
+    <nav
+      className={`pt-7 pl-[3rem] pr-[7rem] flex flex-row fixed w-[100%] z-30 transition-colors duration-300 ${
+        isScrolled ? "bg-[#0e1e70]" : "bg-transparent"
+      }`}>
       <Logo size="3xl" />
 
       <div className="ml-auto w-fit flex gap-x-12 font-semibold text-lg">
