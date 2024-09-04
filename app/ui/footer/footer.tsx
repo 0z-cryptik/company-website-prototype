f01a15259c23b29245413e3040d1ccab4f314222
Eniitan <enitanolawale0@gmail.com>
2024-09-04 09:13:23 +0100

    removed the hard coded date, replaced it with a self reliant one

--- app/ui/footer/footer.tsx
+++ app/ui/footer/footer.tsx
@@ -1,9 +1,11 @@
 export const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
     <div className="mt-[3rem] ml-[7.5%] pb-5 flex flex-row">
       <p className="mt-[-0.15rem]">©</p>
       <p className="font-mono font-thin text-sm ml-2">
-        2024 BlueWave. All rights reserved.
+        {year} BlueWave. All rights reserved.
       </p>
     </div>
   );
